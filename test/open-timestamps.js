8ac01ef4b499b3db99ca40b1df11e07f2c31852d
Riccardo Casatta <riccardo.casatta@gmail.com>
2017-01-10 18:07:27 +0100

    passing messages to assert.fail

--- test/open-timestamps.js
+++ test/open-timestamps.js
@@ -20,8 +20,7 @@ test('setup', assert => {
     incomplete = values[2];
     assert.end();
   }).catch(err=> {
-    console.log("err=" + err);
-    assert.fail();
+    assert.fail("err=" + err);
   });
 });
 
@@ -42,7 +41,6 @@ test('OpenTimestamps.stamp()', assert => {
     assert.equals('05c4f616a8e5310d19d938cfd769864d7f4ccdc2ca8b479b10af83564b097af9', Utils.bytesToHex(detachedTimestampFile.timestamp.msg), 'checking hashes');
     assert.end();
   }).catch(err => {
-    console.log("err=" + err);
-    assert.fail();
+    assert.fail("err=" + err);
   });
 });
