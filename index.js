324ec70bd92a7ba6f3f14d9cafb8490c2a132f59
Riccardo Casatta <riccardo@casatta.it>
2017-07-06 16:49:01 +0200

    adding index

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 const OpenTimestamps = require('./src/open-timestamps.js');
 OpenTimestamps.DetachedTimestampFile = require('./src/detached-timestamp-file.js');
 OpenTimestamps.Ops = require('./src/ops.js');
+OpenTimestamps.Context = require('./src/context.js');
 
 module.exports = OpenTimestamps;
