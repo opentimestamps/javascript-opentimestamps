913b02ac7dc32baf29b576823ccc7664cb23a3fa
Riccardo Casatta <riccardo@casatta.it>
2017-07-18 16:46:08 +0200

    removing test files after testing

--- test/ots-cli.sh
+++ test/ots-cli.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo --- START TESTING ots-cli.js
-node ots-cli.js stamp README.md && node ots-cli.js upgrade examples/empty.ots && node ots-cli.js verify examples/empty.ots && echo --- END TESTING ots-cli.js
+node ots-cli.js stamp README.md && rm README.md.ots && node ots-cli.js upgrade examples/empty.ots && node ots-cli.js verify examples/empty.ots && echo --- END TESTING ots-cli.js
