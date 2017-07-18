23b63a79f4d893500a095f8f1d27012145dd1e1c
Riccardo Casatta <riccardo@casatta.it>
2017-07-18 16:32:16 +0200

    adding shell script to test cli and prevent problem seen in version 0.2.0

---
+++ test/ots-cli.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo --- START TESTING ots-cli.js
+node ots-cli.js stamp README.md && node ots-cli.js upgrade examples/empty.ots && node ots-cli.js verify examples/empty.ots && echo --- END TESTING ots-cli.js
