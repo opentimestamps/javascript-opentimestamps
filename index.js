4fe5247ce390aa93b4a382c5669bf00f483f4133
Luca Vaccaro <luck87@gmail.com>
2017-09-25 13:00:06 +0200

    Add public class

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const OpenTimestamps = require('./src/open-timestamps.js');
 OpenTimestamps.DetachedTimestampFile = require('./src/detached-timestamp-file.js');
 OpenTimestamps.Ops = require('./src/ops.js');
 OpenTimestamps.Context = require('./src/context.js');
+OpenTimestamps.Timestamp = require('./src/timestamp.js');
+OpenTimestamps.Utils = require('./src/utils.js');
+OpenTimestamps.Notary = require('./src/notary.js');
 
 module.exports = OpenTimestamps;
