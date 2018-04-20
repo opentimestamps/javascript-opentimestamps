d169c49ed6148bfd5ce94020a11d79a10a4e05d7
Luca Vaccaro <luck87@gmail.com>
2018-04-20 14:24:44 +0200

    Clear tests

--- test/calendar.js
+++ test/calendar.js
@@ -1,48 +1,10 @@
 const test = require('tape')
 const Calendar = require('../src/calendar.js')
 const Utils = require('../src/utils.js')
-// const Timestamp = require('../src/timestamp.js');
-/*
-let incompleteOtsInfo;
-let incompleteOts;
-let incomplete;
-let helloworldOts;
-let helloworld;
-
-test('setup', assert => {
-  const incompleteOtsInfoPromise = Utils.readFilePromise('./examples/incomplete.txt.ots.info', 'utf8');
-  const incompleteOtsPromise = Utils.readFilePromise('./examples/incomplete.txt.ots', null);
-  const incompletePromise = Utils.readFilePromise('./examples/incomplete.txt', null);
-  const helloworldOtsPromise = Utils.readFilePromise('./examples/hello-world.txt.ots', null);
-  const helloworldPromise = Utils.readFilePromise('./examples/hello-world.txt', null);
-
-  Promise.all([incompleteOtsInfoPromise, incompleteOtsPromise, incompletePromise, helloworldOtsPromise, helloworldPromise]).then(values => {
-    incompleteOtsInfo = values[0];
-    incompleteOts = values[1];
-    incomplete = values[2];
-    helloworldOts = values[3];
-    helloworld = values[4];
-    assert.end();
-  }).catch(err => {
-    assert.fail('err=' + err);
-  });
-});
-*/
-/*
-test('Calendar.submit()', assert => {
-  const calendar = new Calendar.RemoteCalendar('https://finney.calendar.eternitywall.com');
-  const digest = Utils.randBytes(32);
-  calendar.submit(digest).then(timestamp => {
-    assert.true(timestamp !== null);
-    assert.end();
-  }).catch(err => {
-    assert.fail('err=' + err);
-  });
-}); */
 
 test('Calendar.privateSubmit()', assert => {
   const digest = Array.from(Utils.randBytes(32))
-  Utils.readSignatureFile('../signature.wif')
+  Utils.readSignatureFile('./signature.wif')
     .then(hashmap => {
       let calendarUrl = ''
       let signature = ''
