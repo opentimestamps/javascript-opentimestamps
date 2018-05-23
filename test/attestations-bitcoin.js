8743e7ea3b7f96d4577c5e5786def43620da2d53
Luca Vaccaro <luck87@gmail.com>
2018-05-23 15:00:28 +0200

    Remove useless test

--- test/attestations-bitcoin.js
+++ test/attestations-bitcoin.js
@@ -31,29 +31,3 @@ test('sorting', assert => {
   assert.equals(t1.attestations[2].height, 2)
   assert.end()
 })
-
-test('min', assert => {
-  const bitcoinAttestations = [
-    new Notary.BitcoinBlockHeaderAttestation(0),
-    new Notary.BitcoinBlockHeaderAttestation(1),
-    new Notary.BitcoinBlockHeaderAttestation(2)
-  ]
-
-  const t = new Timestamp(Utils.toBytes('abcd', 'UTF-8'))
-  t.attestations.push(bitcoinAttestations[2])
-  t.attestations.push(bitcoinAttestations[1])
-  t.attestations.push(bitcoinAttestations[0])
-
-  assert.equals(t.attestations[0].height, 2)
-  assert.equals(t.attestations[1].height, 1)
-  assert.equals(t.attestations[2].height, 0)
-
-  function min (a, b) { return (a.height < b.height) ? -1 : ((a.height > b.height) ? 1 : 0) }
-  const sorted = t.attestations.sort(min)
-  assert.equals(t.attestations.length, sorted.length)
-  assert.equals(sorted[0].height, 0)
-  assert.equals(sorted[1].height, 1)
-  assert.equals(sorted[2].height, 2)
-
-  assert.end()
-})
