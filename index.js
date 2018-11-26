4e7c8b0444b498fdaabd968855370540615db877
federicoon <federicoon@users.noreply.github.com>
2018-11-26 18:23:24 +0100

    Update index.js
    Need to add Calendar to OpenTimestamps, in order to pass options.whitelist to function OpenTimestamps.verify()

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ OpenTimestamps.DetachedTimestampFile = require('./src/detached-timestamp-file.js
 OpenTimestamps.Ops = require('./src/ops.js')
 OpenTimestamps.Context = require('./src/context.js')
 OpenTimestamps.Timestamp = require('./src/timestamp.js')
+OpenTimestamps.Calendar = require('./src/calendar.js')
 OpenTimestamps.Utils = require('./src/utils.js')
 OpenTimestamps.Notary = require('./src/notary.js')
 
